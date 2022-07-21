d3d2b553ac40608909d49efce09f8a6ccba4c67b
Sam Sharff <samanthaleec@gmail.com>
2022-07-21 15:40:24 -0400

    stores array of chocolate and displays each instance in HTML using a v-for loop; creates input button to add new items to array

--- practice-vuejs/index.js
+++ practice-vuejs/index.js
@@ -5,11 +5,17 @@ var App = {
     return {
       message: "Hello from JavaScript!",
       name: "Sam!",
+      chocolates: ["dark", "darker", "darkest"],
+      newChocolate: "",
     };
   },
   methods: {
     changeMessage: function () {
       this.message = "Whoaaa";
+    },
+    addChocolate: function () {
+      this.chocolates.push(this.newChocolate);
+      this.newChocolate = "";
     },
   },
 };
