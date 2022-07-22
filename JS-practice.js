04055b00551390a6426bea952269d8adaf3b5d89
Sam Sharff <samanthaleec@gmail.com>
2022-07-22 12:10:26 -0400

    practices Math.ceil in a function

--- JS-practice.js
+++ JS-practice.js
@@ -8,3 +8,10 @@ console.log(Math.random() * 100);
 // Math.ceil practice
 
 console.log(Math.ceil(43.8)); // 44
+
+// Math.ceil practice (in a function)
+
+function roundUp(a, b) {
+  return Math.ceil(a + b);
+}
+console.log(roundUp(4.6, 5)); // 10
