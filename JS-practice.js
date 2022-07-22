3dfd56cf0dc40da8a7e9a059b29480dc0aeb2e10
Sam Sharff <samanthaleec@gmail.com>
2022-07-22 11:58:40 -0400

    adds practice with JS methods Math.floor,Mat.random, and Math.ceil

---
+++ JS-practice.js
@@ -0,0 +1,10 @@
+// Math.floor and Math.random practice
+function randomNum() {
+  console.log(Math.floor(Math.random() * 50));
+}
+randomNum();
+console.log(Math.random() * 100);
+
+// Math.ceil practice
+
+console.log(Math.ceil(43.8)); // 44
