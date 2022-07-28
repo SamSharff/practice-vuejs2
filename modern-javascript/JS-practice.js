1d574c4bc91a950d81b117d511b1429ac39cb60d
Sam Sharff <samanthaleec@gmail.com>
2022-07-28 11:47:28 -0400

    practice with Ruby --> JS syntax

--- modern-javascript/JS-practice.js
+++ modern-javascript/JS-practice.js
@@ -39,3 +39,17 @@
 
 // console.log(numbers.reduce(reduceNumbers));
 // *************************
+
+// Write a function that returns the sum of all numbers in a given array.
+
+// NOTE: Do not use any built-in language functions that do this automatically for you.
+
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+// Explanation: (1 + 2 + 3 + 4) = 10
+
+function reduceSum(num, sum); {
+let sum = sum + num;
+};
+reduceSum();
